e6a6978300bfbc239f0df87b38ad07da61de83d0
Rayner33 <rayner233@gmail.com>
2026-06-29 15:45:37 +0200

    style: Usunięcie problematycznej opcji checklisty na życzenie

--- app/src/components/QRModal.jsx
+++ app/src/components/QRModal.jsx
@@ -534,7 +534,7 @@ export default function QRModal({ isOpen, onClose, activeWorkspace, mode = 'crea
                             [{ 'color': ['#FF4C00', '#1ea2e4', '#8B5CF6', '', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444'] }, 
                              { 'background': ['#FF4C00', '#1ea2e4', '#8B5CF6', '', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444'] }],
                             [{ 'align': [] }],
-                            [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+                            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
                             ['link', 'clean']
                           ]
                         }}
