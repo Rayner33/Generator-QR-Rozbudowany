420ca693491117a52cb4e4b8e852a74b87d0bb03
Rayner33 <rayner233@gmail.com>
2026-07-01 19:58:30 +0200

    chore(ui): Przyspieszenie animacji kaskadowej z 80ms do 50ms

--- app/src/utils/animations.js
+++ app/src/utils/animations.js
@@ -28,7 +28,7 @@ export const staggerContainer = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
-    transition: { staggerChildren: 0.08 }
+    transition: { staggerChildren: 0.05 }
   }
 };
 
